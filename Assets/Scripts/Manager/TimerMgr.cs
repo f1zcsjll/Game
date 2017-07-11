18c0d63555741a95a7b50bf1518c229132457c61
f1zcsjll <f1zcsjll@gmail.com>
2017-07-11 16:14:13 +0800

    增加定时器

---
+++ Assets/Scripts/Manager/TimerMgr.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tools;
+
+namespace Manager
+{
+    public class TimerMgr
+    {
+        private static TimerMgr _instance;
+        private Dictionary<int, Timer> TimerList = new Dictionary<int, Timer>();
+        public static TimerMgr GetInstance()
+        {
+            if (_instance == null)
+                _instance = new TimerMgr();
+            return _instance;
+        }
+
+        /// <summary>
+        /// 添加定时器
+        /// </summary>
+        /// <param name="delaytime">延迟执行时间</param>
+        /// <param name="interval">重复间隔时间，为0不重复</param>
+        /// <param name="times">重复执行次数，为0无限执行</param>
+        /// <param name="action">执行函数</param>
+        public int AddTimer(MVC.Notifier.StandardDelegate action, float delaytime=0, float interval = 0, int times = 0)
+        {
+            Timer newtimer = new Timer(action, delaytime, interval, times);
+            if(newtimer!=null)
+            {
+                int key=newtimer.GetHashCode();
+                TimerList.Add(key, newtimer);
+                return key;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// 删除定时器
+        /// </summary>
+        /// <param name="key"></param>
+        public void RemoveTimer(int key)
+        {
+            if(TimerList.ContainsKey(key))
+            {
+                TimerList[key].StopTimer();
+                TimerList.Remove(key);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/Timer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools
+{
+    public class Timer :MonoBehaviour
+    {
+        private float DelayTime = 0;
+        private float Interval = 0;
+        private int Times = 0;
+        private MVC.Notifier.StandardDelegate Action;
+
+        /// <summary>
+        /// 初始化定时器
+        /// </summary>
+        /// <param name="delaytime">延迟执行时间</param>
+        /// <param name="interval">重复间隔时间，为0不重复</param>
+        /// <param name="times">重复执行次数，为0无限执行</param>
+        /// <param name="action">执行函数</param>
+        public Timer(MVC.Notifier.StandardDelegate action, float delaytime=0, float interval = 0, int times = 0)
+        {
+            DelayTime = delaytime;
+            Interval = interval;
+            Times = times;
+            Action = action;
+            if(Interval==0)
+            {
+                Invoke(Action.Method.Name, DelayTime);
+            }
+            else if(Times==0)
+            {
+                InvokeRepeating(Action.Method.Name, DelayTime, Interval);
+            }
+            else
+            {
+                Invoke("TimesAction", DelayTime);
+            }
+        }
+
+        void TimesAction()
+        {
+            for(int i=Times-1;i>=0;i--)
+            {
+                StartCoroutine(Do(i * Interval));
+            }
+        }
+
+        IEnumerator Do(float waittime)
+        {
+            yield return new WaitForSeconds(waittime);
+            Action();
+            CancelInvoke("TimesAction");
+        }
+
+        /// <summary>
+        /// 停止计时器
+        /// </summary>
+        public void StopTimer()
+        {
+            CancelInvoke(Action.Method.Name);
+            CancelInvoke("TimesAction");
+            StopAllCoroutines();
+        }
+    }
+}
