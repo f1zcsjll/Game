06715f603cd52ac2497d7d88473fd6bc768f3d3f
f1zcsjll <f1zcsjll@gmail.com>
2017-07-17 19:45:44 +0800

    增加临时数据保存

--- Assets/Scripts/Manager/DataMgr.cs
+++ Assets/Scripts/Manager/DataMgr.cs
@@ -22,5 +22,46 @@ namespace Manager
             return _instance;
         }
        
+        /// <summary>
+        /// 保存玩家数据
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="data"></param>
+        public void SavePrefsData(string name,object data)
+        {
+            PlayerPrefs.SetString(name,(string)data.ToString());
+        }
+
+        /// <summary>
+        /// 获取玩家数据
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public object GetPrefsData(string name,string defaultvalue="")
+        {
+            return PlayerPrefs.GetString(name,defaultvalue);
+        }
+
+        public void SaveAllPrefsData()
+        {
+            PlayerPrefs.Save();
+        }
+       
+        /// <summary>
+        /// 清除玩家某项数据
+        /// </summary>
+        /// <param name="name"></param>
+        public void CleanPrefsData(string name)
+        {
+            PlayerPrefs.DeleteKey(name);
+        }
+
+        /// <summary>
+        /// 清除所有玩家数据
+        /// </summary>
+        public void CleanAllPrefsData()
+        {
+            PlayerPrefs.DeleteAll();
+        }
     }
 }
