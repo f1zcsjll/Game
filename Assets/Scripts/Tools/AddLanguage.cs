33003d2c9c2fed53196fe97ddec64270c381b1f7
f1zcsjll <f1zcsjll@gmail.com>
2017-07-17 11:34:11 +0800

    更新Localization

---
+++ Assets/Scripts/Tools/AddLanguage.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Tools
+{
+    public class AddLanguage : EditorWindow
+    {
+        static private Dictionary<string, string> DicLocalization = new Dictionary<string, string>();
+        static private List<string> language = new List<string>();
+        static private string addlanguage = "";
+        static private string path = Localization.Path;
+
+        [MenuItem("Tools/添加语言")]
+        static void OpenWinAddLoc()
+        {
+            ReadLocalization();
+            GetWindowWithRect(typeof(AddLanguage), new Rect(0, 0, 400, 100), true, "添加语言");
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("语言名称：", GUILayout.Width(60));
+            addlanguage = EditorGUILayout.TextField(addlanguage);
+            GUILayout.EndHorizontal();            
+            GUILayout.Space(5);
+            if (GUILayout.Button("添加"))
+            {
+                if (string.IsNullOrEmpty(addlanguage))
+                {
+                    EditorUtility.DisplayDialog("", "语言不能为空", "ok");
+                }
+                else if (language.Contains(addlanguage))
+                {
+                    EditorUtility.DisplayDialog("", "该语言已存在", "ok");
+                }
+                else
+                {
+                    ChangeLocalization();
+                    SaveLocalization();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 读取Localization
+        /// </summary>
+        static void ReadLocalization()
+        {
+            DicLocalization.Clear();
+            if (File.Exists(path))
+            {
+                StreamReader sr = new StreamReader(path, Encoding.UTF8);
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (language.Count == 0)
+                    {
+                        language = new List<string>(line.Split(','));
+                    }
+                    string[] data = line.Split(new[] { ',' }, StringSplitOptions.None);
+                    DicLocalization.Add(data[0], line);
+                }
+                sr.Close();
+            }
+        }
+
+        /// <summary>
+        /// 修改Localization
+        /// </summary>
+        static void ChangeLocalization()
+        {
+            bool isfirst = true;
+            var e = new List<string>(DicLocalization.Keys);
+            for (int i = 0; i < e.Count;i++)
+            {
+                if (isfirst)
+                {
+                    DicLocalization[e[i]] += "," + addlanguage;
+                    language.Add(addlanguage);
+                    isfirst = false;
+                }
+                else
+                {
+                    DicLocalization[e[i]] += ",";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 保存Localization
+        /// </summary>
+        static void SaveLocalization()
+        {
+            StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8);
+            var e = DicLocalization.GetEnumerator();
+            while (e.MoveNext())
+            {
+                sw.WriteLine(e.Current.Value);
+            }
+            e.Dispose();
+            sw.Close();
+            EditorUtility.DisplayDialog("", "添加成功", "ok");
+        }
+    }
+}
--- Assets/Scripts/Tools/Localization.cs
+++ Assets/Scripts/Tools/Localization.cs
@@ -8,13 +8,18 @@ public static class Localization
     static private Dictionary<string, string[]> DicLocalization = new Dictionary<string, string[]>();
     static private int LanguageType = 1;
     static private string[] LanguageList = new string[0];
+    public static readonly string Path = Application.dataPath + "/Resources/" + Manager.PathMgr.GetInstance().GetPath(Define.DataType.Localization) + ".txt";
 
     private static void Init()
     {
         if(DicLocalization.Count==0)
         {
             AssetMgr.GetInstance().LoadAsset(PathMgr.GetInstance().GetPath(Define.DataType.Localization),false,false,()=>{
-                string[] loc = AssetMgr.GetInstance().GetAsset().ToString().Split('\n');
+                string[] loc = AssetMgr.GetInstance().GetAsset().ToString().Split(new[]{'\n'},System.StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < loc.Length;i++)
+                {
+                    loc[i] = loc[i].Replace("\r", "");
+                }
                 LanguageList = loc[0].Split(',');
                 for(int i=1;i<loc.Length;i++)
                 {
@@ -36,7 +41,31 @@ public static class Localization
         if(DicLocalization.ContainsKey(key))
         {
             if (!string.IsNullOrEmpty(DicLocalization[key][LanguageType]))
-                return DicLocalization[key][LanguageType].Replace("\\n","\n");
+            {                
+                if (DicLocalization[key][LanguageType].Contains("[b-]"))
+                {
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[b-]", "</b>");
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[b]", "<b>");
+                }
+                if (DicLocalization[key][LanguageType].Contains("[i-]"))
+                {
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[i-]", "</i>");
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[i]", "<i>");
+                }
+                if (DicLocalization[key][LanguageType].Contains("[c-]"))
+                {
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[c-", "</color");
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[c", "<Color=#");
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("]", ">");
+                }
+                if (DicLocalization[key][LanguageType].Contains("[s-]"))
+                {
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[s-", "</size");
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("[s", "<size=");
+                    DicLocalization[key][LanguageType] = DicLocalization[key][LanguageType].Replace("]", ">");
+                }
+                return DicLocalization[key][LanguageType].Replace("\\n", "\n");
+            }
             else
                 return key;
         }
