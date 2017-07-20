c4142022c201ba0e836bd7ff19345b59c1f8c62d
f1zcsjll <f1zcsjll@gmail.com>
2017-07-20 18:17:14 +0800

    优化UI管理器

--- Assets/Scripts/Manager/UIMgr.cs
+++ Assets/Scripts/Manager/UIMgr.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Define;
 using Base;
+using System;
 
 namespace Manager
 {
@@ -22,9 +23,9 @@ namespace Manager
             UIRoot.GetComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
             UIRoot.GetComponent<CanvasScaler>().referenceResolution = new Vector2(Screen.width, Screen.height);
             UIRoot.GetComponent<CanvasScaler>().screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
-            Object.DontDestroyOnLoad(UIRoot);
+            UnityEngine.Object.DontDestroyOnLoad(UIRoot);
             EventSystem = new GameObject("EventSystem", typeof(EventSystem), typeof(StandaloneInputModule));
-            Object.DontDestroyOnLoad(EventSystem);
+            UnityEngine.Object.DontDestroyOnLoad(EventSystem);
         }
         public static UIMgr GetInstance()
         {
@@ -33,7 +34,12 @@ namespace Manager
             return _instance;
         }
 
-        public void ShowWin(UIType type)
+        /// <summary>
+        /// 打开界面
+        /// </summary>
+        /// <param name="type">界面类型</param>
+        /// <param name="funtion">完成后回调</param>
+        public void ShowWin(UIType type,Action funtion=null)
         {
             if (!HasWinOpen(type))
             {
@@ -48,35 +54,84 @@ namespace Manager
             {
                 WinList[type].gameObject.SetActive(true);
             }
+            if (funtion != null)
+                funtion();
         }
 
-        public void CloseWin(UIType type)
+        /// <summary>
+        /// 获取已打开的界面的Object
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public GameObject GetWin(UIType type)
+        {
+            if(HasWinOpen(type))
+            {
+                return WinList[type].gameObject;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 关闭界面
+        /// </summary>
+        /// <param name="type">界面类型</param>
+        /// <param name="funtion">完成后回调</param>
+        public void CloseWin(UIType type, Action funtion = null)
         {
             if (HasWinOpen(type))
             {
                 GameObject.Destroy(WinList[type].gameObject);
                 WinList.Remove(type);
+                funtion();
             }
         }
 
+        /// <summary>
+        /// 某界面是否打开
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public bool HasWinOpen(UIType type)
         {
             return WinList.ContainsKey(type);
         }
 
-        public void HideWin(UIType type)
+        /// <summary>
+        /// 隐藏界面
+        /// </summary>
+        /// <param name="type">窗口类型</param>
+        /// <param name="isHide">是否隐藏</param>
+        /// <param name="funtion">完成后回调</param>
+        public void HideWin(UIType type, bool isHide, Action funtion = null)
         {
             if(HasWinOpen(type))
             {
-                WinList[type].gameObject.SetActive(false);
+                WinList[type].gameObject.SetActive(!isHide);
+                funtion();
             }
         }
 
-        public void HideAllWin()
+        /// <summary>
+        /// 隐藏所有界面，保留过滤列表里面的界面
+        /// </summary>
+        /// <param name="isHide"></param>
+        public void HideAllWin(bool isHide, List<UIType> filter)
         {
-            UIRoot.SetActive(false);
+            List<UIType> closelist = new List<UIType>(WinList.Keys);
+            var e = closelist.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (!filter.Contains(e.Current))
+                    HideWin(e.Current, true);
+            }
+            e.Dispose();
+            closelist.Clear();
         }
 
+        /// <summary>
+        /// 关闭所有界面
+        /// </summary>
         public void CloseAllWin()
         {
             List<UIType> closelist = new List<UIType>(WinList.Keys);
@@ -89,6 +144,10 @@ namespace Manager
             closelist.Clear();
         }
 
+        /// <summary>
+        /// 关闭所有界面，保留过滤列表里面的界面
+        /// </summary>
+        /// <param name="filter"></param>
         public void CloseWinByFilter(List<UIType> filter)
         {
             List<UIType> closelist = new List<UIType>(WinList.Keys);
