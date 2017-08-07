1bb2d53bd4b097ebc30841fbd3d6229bf5db56c0
f1zcsjll <f1zcsjll@gmail.com>
2017-08-07 12:29:39 +0800

    目录拆分

--- Assets/Scripts/Editor/AddLanguage.cs
+++ Assets/Scripts/Editor/AddLanguage.cs
@@ -6,7 +6,7 @@ using System;
 using System.IO;
 using System.Text;
 
-namespace Tools
+namespace Tools.Editor
 {
     public class AddLanguage : EditorWindow
     {
---
+++ Assets/Scripts/GameEnter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager;
+
+public class GameEnter : MonoBehaviour {
+
+	void Start () {
+        FrameMgr.GetInstance();
+        TimerMgr.GetInstance();
+        PathMgr.GetInstance();
+        ConfigMgr.GetInstance();
+        AssetMgr.GetInstance();
+        UIMgr.GetInstance();
+        SoundMgr.GetInstance();
+        LoadSceneMgr.LoadSence("test");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
